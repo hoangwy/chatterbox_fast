8f75045cdc53eefa90e21d3e51a9cdc0207fe580
hoang quy <hoang.wy@gmail.com>
2025-11-07 23:00:24 +0700

    Update example_tts.py

--- example_tts.py
+++ example_tts.py
@@ -14,7 +14,7 @@ print(f"Using device: {device}")
 
 model = ChatterboxTTS.from_pretrained(device=device)
 
-text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
+text = "On November seventh, Air France-KLM announced its intention to formally bid for a significant stake in TAP Air Portugal. This move comes as the Portuguese government plans to privatize a portion of its national airline.\n\nAir France-KLM's CEO, Ben Smith, shared this news during a press conference where the company presented its quarterly financial results. This confirms earlier speculation that the airline group was interested in TAP.\n\nMeanwhile, the Portuguese government confirmed nearly two years ago, in September two thousand twenty-three, its plan to privatize part of TAP. The government aims to sell forty-four point nine percent to a private investor and five percent to employees, while retaining a fifty point one percent majority stake.\n\nAccording to officials, interested parties must submit their proposals by November twenty-second. This follows a long history of TAP moving between state and private ownership, with the government fully taking over again during the COVID-nineteen pandemic.\n\nEarlier this year, in July, the government approved the law outlining TAP's privatization, stating its goal was to stop \"pouring money into a bottomless pit.\" This upcoming bid from Air France-KLM marks a significant step in that process.",
 wav = model.generate(text)
 ta.save("test-1.wav", wav, model.sr)
 
